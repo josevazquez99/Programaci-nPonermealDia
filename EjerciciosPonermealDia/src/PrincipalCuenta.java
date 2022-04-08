4d6fe7b75143c3fde015f3e3b50df6d30d510e4a
Jose Antonio Vazquez <98308411+josevazquez99@users.noreply.github.com>
2022-04-08 09:00:38 +0200

    PrincipalCuenta

--- EjerciciosPonermealDia/src/PrincipalCuenta.java
+++ EjerciciosPonermealDia/src/PrincipalCuenta.java
@@ -2,31 +2,29 @@ package CuentaCorriente;
 
 import java.util.Scanner;
 
-public class PrincipalCuenta {
+public class PrincipalCuenta{
 	public static Scanner teclado = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Cuenta c = new Cuenta(20);
-		String texto;
+		int texto;
 		System.out.println("Que operación desea realizar");
-		texto = teclado.nextLine();
+		texto = Integer.parseInt(teclado.nextLine());
 
-		while (texto != "4") {
-			if (texto == "1") {
+		while (texto != 4) {
+			if (texto == 1) {
 				System.out.println("Cual es la cantidad que desea retirar");
-				texto=teclado.nextLine();
-				c.setReintegro(0);
+				texto = Integer.parseInt(teclado.nextLine());
+				c.setReintegro(texto);
 				System.out.println(c.getSaldo());
-			} else if (texto == "2") {
+			} else if (texto == 2) {
 				System.out.println(" Que cantidad deseas ingresar ");
-				texto=teclado.nextLine();
-				c.setIngreso(0);
+				texto = Integer.parseInt(teclado.nextLine());
+				c.setIngreso(texto);
 				System.out.println(c.getSaldo());
-			} else if (texto == "3") {
+			} else if (texto == 3) {
 				System.out.println(c.getSaldo());
-				System.out.println(c.getContadoringreso());
-				System.out.println(c.getContadoringreso());
 			}
 
 		}
