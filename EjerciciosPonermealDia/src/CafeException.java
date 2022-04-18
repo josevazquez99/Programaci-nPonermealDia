d9647e94f935450c5ea41849e943dcde5bdcd9b2
Jose Antonio Vazquez <98308411+josevazquez99@users.noreply.github.com>
2022-04-18 17:35:56 +0200

    Add files via upload

---
+++ EjerciciosPonermealDia/src/CafeException.java
@@ -0,0 +1,30 @@
+package Cafe;
+
+public class CafeException extends Exception {
+
+	public CafeException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CafeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CafeException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CafeException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CafeException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
