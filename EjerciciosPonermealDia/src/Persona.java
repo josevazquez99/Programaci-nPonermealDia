44f66de72c11d21c2a2a325b8367cd852d22d918
Jose Antonio Vazquez <98308411+josevazquez99@users.noreply.github.com>
2022-05-03 10:06:55 +0200

    Update Persona.java

--- EjerciciosPonermealDia/src/Persona.java
+++ EjerciciosPonermealDia/src/Persona.java
@@ -1,5 +1,7 @@
 package persona;
 
+import java.util.Random;
+
 public class Persona {
 	private final static char sexodefenido = 'H';
 	public static final int infrapeso = -1;
@@ -7,12 +9,15 @@ public class Persona {
 	public static final int sobrepeso = 1;
 	private String nombre;
 	private int edad;
-	private String DNI;
+	private String dni;
 	private char sexo;
 	private double peso;
 	private double altura;
+
 	public Persona() {
 		super();
+		generaDni();
+		comprobarSexo();
 	}
 
 	public Persona(String nombre, int edad, char sexo) {
@@ -20,15 +25,17 @@ public class Persona {
 		this.nombre = nombre;
 		this.edad = edad;
 		this.sexo = sexo;
+		generaDni();
+		comprobarSexo();
 
 	}
 
-	public Persona(String nombre, int edad, char sexo, double peso,double altura) {
+	public Persona(String nombre, int edad, char sexo, double peso, double altura) {
 		this.nombre = nombre;
 		this.edad = edad;
 		this.peso = peso;
 		this.altura = altura;
-		generarDni();
+		generaDni();
 		this.sexo = sexo;
 		comprobarSexo();
 	}
@@ -39,19 +46,34 @@ public class Persona {
 		}
 	}
 
-	private void generarDni() {
-		final int divisor = 23;
-		int numDNI = ((int) Math.floor(Math.random() * (100000000 - 10000000) + 10000000));
-		int res = numDNI - (numDNI / divisor * divisor);
-		char letraDNI = generaLetraDNI(res);
-		DNI = Integer.toString(numDNI) + letraDNI;
+	private String generaLetraDni(String numerosDni) {
+		String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+		String resultado = numerosDni;
+		int asignarLetra = Integer.parseInt(resultado) % 23;
+		resultado += letras.charAt(asignarLetra);
+		return resultado;
 	}
 
-	private char generaLetraDNI(int res) {
-		char letras[] = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H',
-				'L', 'C', 'K', 'E' };
+	private String generaNumero() {
+		Random dni = new Random();
+		int randomAscii;
+		String resultado = "";
+		for (int i = 0; i < 8; i++) {
+			randomAscii = dni.nextInt(9) + 1;
+			resultado = resultado + randomAscii;
+		}
+		return resultado;
+	}
 
-		return letras[res];
+	private String generaDni() {
+		String resultadoDni = "", numerosDni;
+		numerosDni = generaNumero();
+		resultadoDni += generaLetraDni(numerosDni);
+		return resultadoDni;
+	}
+
+	public String getdni() {
+		return dni;
 	}
 
 	public void setNombre(String nombre) {
@@ -102,7 +124,7 @@ public class Persona {
 			sexo = "mujer";
 		}
 		return "Informacion de la persona:\n" + "Nombre: " + nombre + "\n" + "Sexo: " + sexo + "\n" + "Edad: " + edad
-				+ " años\n" + "DNI: " + DNI + "\n" + "Peso: " + peso + " kg\n" + "Altura: " + altura + " metros\n";
+				+ " años\n" + "DNI: " + dni + "\n" + "Peso: " + peso + " kg\n" + "Altura: " + altura + " metros\n";
 	}
 
 }
