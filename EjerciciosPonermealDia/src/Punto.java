a32b38e720ee6a61dcf95e30b84f8a979eee2430
Jose Antonio Vazquez <98308411+josevazquez99@users.noreply.github.com>
2022-05-03 09:46:07 +0200

    Add files via upload

---
+++ EjerciciosPonermealDia/src/Punto.java
@@ -0,0 +1,37 @@
+package Relacion7;
+
+public class Punto {
+	private double x;
+	private double y;
+
+	public Punto() {
+		super();
+	}
+
+	public Punto(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y =y;
+	}
+
+	@Override
+	public String toString() {
+		return "Punto [x=" + x + ", y=" + y + "]";
+	}
+
+}
